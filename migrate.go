c6aa825819caf27d4fdf2bc969ecf861e06811c9
trueifnotfalse <trueifnotfalse@gmail.com>
2025-01-21 14:43:37 +0700

    clickhouse

--- migrate.go
+++ migrate.go
@@ -1,9 +1,14 @@
 package migrate
 
 import (
+	"github.com/trueifnotfalse/golang-migrate/database/clickhouse"
 	"github.com/trueifnotfalse/golang-migrate/database/postgres"
 )
 
 func Postgres(dsn string) *postgres.Migrator {
 	return postgres.New(dsn)
+}
+
+func Clickhouse(dsn string) *clickhouse.Migrator {
+	return clickhouse.New(dsn)
 }
